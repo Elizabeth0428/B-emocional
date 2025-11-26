0953db3b30cafb2b9ee8bf51aa48e6d9ca9c392e
Elizabeth0428 <lizma883@gmail.com>
2025-11-26 01:39:25 -0600

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -16,12 +16,25 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
+//export const db = await mysql.createPool({
+  //host: process.env.DB_HOST,
+  //user: process.env.DB_USER,
+  //password: process.env.DB_PASSWORD,
+  //database: process.env.DB_NAME,
+  //port: process.env.DB_PORT,
+  //waitForConnections: true,
+  //connectionLimit: 10,
+  //queueLimit: 0,
+  //ssl: {
+    //rejectUnauthorized: false
+  //}
+//});
 export const db = await mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: mysql.railway.internal,
+  user: root,
+  password: pmyevOYtoEIhVmpsRzvpyRyLDbtHuhhI,
+  database: railway,
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
