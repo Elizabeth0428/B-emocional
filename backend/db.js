04b880f7ce7798e380fafb758270fc3617cd6b46
Elizabeth0428 <lizma883@gmail.com>
2025-11-26 03:12:48 -0600

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -43,14 +43,3 @@ export const db = await mysql.createPool({
   }
 });
 
-export async function verificarConexion() {
-  try {
-    const [rows] = await db.query("SELECT 1");
-    console.log("Conexión exitosa a la base de datos");
-    return true;
-  } catch (error) {
-    console.error("Error conectando a la base de datos:", error);
-    return false;
-  }
-}
-
